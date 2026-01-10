1ac66cb502fc6c9e2d268c5e4018cd01b7548e35
MauricioDiasSilva <136522388+MauricioDiasSilva@users.noreply.github.com>
2026-01-10 19:49:24 -0300

    Fix: Enable media serving in production

--- config/urls.py
+++ config/urls.py
@@ -10,3 +10,13 @@ urlpatterns = [
     path('add-product/', add_product, name='add_product'),
     path('<slug:slug>/', product_detail, name='product_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Force media serving in production (since we are not using GCS)
+from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
