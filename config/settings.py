3a2b164654dad19f4573785e5e2b7ea34211c474
MauricioDiasSilva <136522388+MauricioDiasSilva@users.noreply.github.com>
2026-01-18 21:45:59 -0300

    Fix CSRF and SSL Security

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,13 @@ CSRF_TRUSTED_ORIGINS = [
     'https://www.dilvaniateixeira.com.br',
 ]
 
+# Cloud Run / Load Balancer SSL Support
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
 
 # Application definition
 
